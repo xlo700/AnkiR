cdd78672eec368552dea194cef2ca8e1c9278cdb
xlo700 <FYY51694@hotmail.com>
2026-02-24 20:21:59 +0800

    updata version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "me.xlo.ankir"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
